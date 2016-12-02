c433a153478d30ac2b7dfe709d67fbe450fb8f25
elmec22z <elmec22z@mtholyoke.edu>
2016-12-02 01:35:00 -0500

    static

--- server/src/server.js
+++ server/src/server.js
@@ -5,16 +5,19 @@ var reverseString = util.reverseString;
 var express = require('express');
 // Creates an Express server.
 var app = express();
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
 // Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-res.send('Hello World!');
-});
+//app.get('/', function (req, res) {
+//res.send('Hello World!');
+//});
 app.use(bodyParser.text());
 // Starts the server on port 3000!
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
 });
-// Handle POST /reverse [data]
+/*/ Handle POST /reverse [data]
 app.post('/reverse', function (req, res) {
 // How do we get the input text?
 // How do we send the output text?
@@ -25,4 +28,4 @@ res.send(reversed);
 // POST did not contain a string. Send an error code back!
 res.status(400).end()
 }
-});
+});*/
